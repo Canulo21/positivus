784a99537df5dd6cdfbafc2a84e3d1cee7972767
JC#21 <canulo1234@gmail.com>
2025-02-26 17:36:06 +0800

    updater framer-motion

--- src/variants.js
+++ src/variants.js
@@ -3,15 +3,15 @@ export const fadeIn = (direction, delay) => {
     hidden: {
       y: direction === "up" ? 40 : direction === "down" ? -40 : 0,
       x: direction === "left" ? 40 : direction === "right" ? -40 : 0,
-      opacity: 0, // Initial opacity set to 0
+      opacity: 0,
     },
     show: {
       y: 0,
       x: 0,
-      opacity: 1, // Change opacity to 1
+      opacity: 1,
       transition: {
         type: "tween",
-        duration: 1.2,
+        duration: 0.6, // Reduced duration for faster animation
         delay: delay,
         ease: [0.25, 0.25, 0.25, 0.75],
       },
